52c5aa8754bc2c22661079b80fd0146af40a885c
Vladimir <faimper@gmail.com>
2020-03-27 07:57:22 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRgister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'mrbl'
+# // создаёт новую точку входа, /auth 
+jwt = JWT(app, authenticate, identity) 
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRgister, '/register')
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+
+    app.run(port=5000, debug=True)
